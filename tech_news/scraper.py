8e9349c0faaac77fcc1616b55a11d1681e895efb
marceloalexandredacunhasimao <marceloalexandredacunhasimao@gmail.com>
2022-11-08 19:35:06 -0300

    feat: req-04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,7 +30,32 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    comments_count = selector.css("#comments .title-block::text").get()
+    if not comments_count:
+        comments_count = 0
+    else:
+        comments_count = int(comments_count.strip().split(" ")[0])
+    # summary = selector.css(".entry-content p::text").get().strip()
+    summary = selector.css(".entry-content > p:first-of-type *::text").getall()
+    summary = "".join(summary).strip()
+    # print("<<<<<"+summary+">>>>>")
+    tags = selector.css(".post-tags ul li a::text").getall()
+    category = selector.css("span.label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5

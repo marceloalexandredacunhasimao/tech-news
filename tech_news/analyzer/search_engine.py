70deacc167917bb167db0e4e39368fb318d219cd
marceloalexandredacunhasimao <marceloalexandredacunhasimao@gmail.com>
2022-11-08 22:41:58 -0300

    feat: requisitos 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,26 @@
+import datetime
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    complete_news = search_news(query)
+    return [(news["title"], news["url"]) for news in complete_news]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        search_date = datetime.date.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data inválida")
+    search_date = search_date.strftime("%d/%m/%Y")
+    query = {"timestamp": search_date}
+    complete_news = search_news(query)
+    return [(news["title"], news["url"]) for news in complete_news]
+    
+
 
 
 # Requisito 8

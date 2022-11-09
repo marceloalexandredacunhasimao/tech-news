0378a0830ca4e88622ae0a0375ebdb3d6b721aad
marceloalexandredacunhasimao <marceloalexandredacunhasimao@gmail.com>
2022-11-09 01:28:39 -0300

    feat: req-11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,4 @@
-from tech_news.database import get_collection
+from tech_news.database import get_collection, find_news
 
 
 # Requisito 10
@@ -10,4 +10,15 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    count = {}
+    news_list = find_news()
+    categories = [news["category"] for news in news_list]
+    for category in categories:
+        if category not in count:
+            count[category] = 0
+        count[category] += 1
+    count = list(count.items())
+    if len(count) == 0:
+        return []
+    count = sorted(count, key=lambda c: (-c[1], c[0]))
+    return [category for category, _ in count]

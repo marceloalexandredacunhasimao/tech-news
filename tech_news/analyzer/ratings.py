c680e8ab87c63352e1c37f6c4c295ad79e12837b
marceloalexandredacunhasimao <marceloalexandredacunhasimao@gmail.com>
2022-11-09 00:30:15 -0300

    feat: req-10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,11 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    query = [("comments_count", -1), ("title", 1)]
+    complete_news = get_collection().find({}).sort(query).limit(5)
+    return [(news["title"], news["url"]) for news in complete_news]
 
 
 # Requisito 11
